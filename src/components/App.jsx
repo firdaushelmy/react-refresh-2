2f3e1d923e80eed85f96feccde142acad0661382
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-19 01:01:53 +0800

    connect css to card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 function App() {
   return (
     <div>
-      <h1>My Contacts</h1>
+      <h1 className='heading'>My Contacts</h1>
       <Card
         name='Beyonce'
         img='https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg'
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 function Card(props) {
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <img
-        src={props.img}
-        alt={props.alt}
-      />
-      <p>{props.phoneNumber}</p>
-      <p>{props.email}</p>
+    <div className='card'>
+      <div className='top'>
+        <h2 className='name'>{props.name}</h2>
+        <img className='circle-img'
+          src={props.img}
+          alt={props.alt}
+        />
+      </div>
+      <div className='bottom'>
+        <p className='bottom info'>{props.phoneNumber}</p>
+        <p className='bottom info'>{props.email}</p>
+      </div>
     </div>
   )
 }
