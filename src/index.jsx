ab4354ded53837cb355556f7e011b8e16275eac3
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-19 00:30:53 +0800

    add one more contact + complete App function and connect to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,38 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img
-        src={props.img}
-        alt={props.alt}
-      />
-      <p>{props.phoneNumber}</p>
-      <p>{props.email}</p>
-    </div>
-  )
-}
-
+import App from "./components/App"
 
 ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name='Beyonce'
-      img='https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg'
-      alt='beyonce_nceavatar_img'
-      phoneNumber='+123 456 789'
-      email='b@beyonce.com' />
-    <Card
-      name='Jack Bauer'
-      img='https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg'
-      alt='jackBauer_avatar_img'
-      phoneNumber='+987 654 321'
-      email='jack@nowhere.com' />
-    <Card />
-
-  </div>,
+  <App />,
   document.getElementById("root")
 );
