7e343bd7dbf8c4c090aca3017aa53d3ab58baa50
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-19 16:44:22 +0800

    connect contacts to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import Card from './Card'
+import contacts from './contacts'
 
 function App() {
   return (
     <div>
       <h1 className='heading'>My Contacts</h1>
       <Card
-        name='Beyonce'
-        img='https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg'
-        alt='beyonce_nceavatar_img'
-        phoneNumber='+123 456 789'
-        email='b@beyonce.com' />
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        phoneNumber={contacts[0].phone}
+        email={contacts[0].email} />
       <Card
-        name='Jack Bauer'
-        img='https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg'
-        alt='jackBauer_avatar_img'
-        phoneNumber='+987 654 321'
-        email='jack@nowhere.com' />
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        phoneNumber={contacts[1].phone}
+        email={contacts[1].email} />
       <Card
-        name='Chuck Norris'
-        img='https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png'
-        alt='chucks_avatar_img'
-        phoneNumber='+918 372 574'
-        email='gmail@chucknorris.com' />
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        phoneNumber={contacts[2].phone}
+        email={contacts[2].email} />
     </div>
   )
 }
