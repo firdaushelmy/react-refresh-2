b55b51b215bf5294d74e77086cabc934818f0ef9
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-07-19 23:51:07 +0800

    refactor hardcoded contacts array into .map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,26 +3,23 @@ import Card from './Card'
 import contacts from './contacts'
 import Avatar from './Avatar'
 
+function createCard(contacts) {
+  return (
+    <Card
+      name={contacts.name}
+      img={contacts.imgURL}
+      phoneNumber={contacts.phone}
+      email={contacts.email}
+    />
+  )
+}
+
 function App() {
   return (
     <div>
       <h1 className='heading'>My Contacts</h1>
       <Avatar img='https://beamstart.com/API/v6.0/index.php?system=images&type=avatar&profileid=254394&width=960' />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phoneNumber={contacts[0].phone}
-        email={contacts[0].email} />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phoneNumber={contacts[1].phone}
-        email={contacts[1].email} />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phoneNumber={contacts[2].phone}
-        email={contacts[2].email} />
+      {contacts.map(createCard)}
     </div>
   )
 }
